9df2db81aa614c49844b0629e844664fdf14b0c7
NawazGhori <mohammednawaz24@gmail.com>
2021-06-06 21:34:09 +0530

    product screen view implementation

--- ecommerce/src/screens/ProductScreen.tsx
+++ ecommerce/src/screens/ProductScreen.tsx
@@ -1,50 +1,118 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { match, NavLink } from 'react-router-dom';
 import getDetails from "../actions/DetailActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import Rating from "../components/Rating";
 
-interface IState{}
-interface IProps{
+interface IState {
+    qty: number;
+}
+
+interface IProps {
     match: match<paramRoutes>;
-    getDetailsById:any;
-    res:any;
+    getDetailsById: any;
+    res: any;
 }
 
-interface paramRoutes{
-    id:any;
+interface paramRoutes {
+    id: any;
 }
 
-class ProductScreen extends Component<IProps,IState>{
-    constructor(props:IProps){
+class ProductScreen extends Component<IProps, IState>{
+    constructor(props: IProps) {
         super(props);
+        this.state = {
+            qty: 1
+        }
     }
-    componentDidMount(){
+
+    setQty = (num: any) => {
+        this.setState({
+            qty: num
+        })
+    }
+
+    componentDidMount() {
         this.props.getDetailsById(this.props.match.params.id);
     }
-    render(){
-        const {loading,product,error} = this.props.res
-        return(
+    render() {
+        const { loading, product, error } = this.props.res
+        return (
             <React.Fragment>
-                <NavLink to="/" className="back_screen"><i className="fa fa-home"></i></NavLink>
-                <p>{this.props.match.params.id}</p>
-                <p>{JSON.stringify(loading)}...{JSON.stringify(product)}.... {JSON.stringify(error)}</p>
+                {
+                    !loading ? (<LoadingBox></LoadingBox>) : error == "Network Error" ? (<MessageBox variant="danger">{error}</MessageBox>) : (<div>
+                        <NavLink to="/" className="back_screen"><i className="fa fa-home"></i></NavLink>
+
+                        <div className="row top">
+
+                            <div className="col-2">
+                                <img src={product.image} alt={product.name} />
+                            </div>
+
+                            <div className="col-1">
+                                <ul>
+                                    <li><h2>{product.name}</h2></li>
+                                    <li><Rating rating={product.rating} numReviews={product.numReviews}></Rating></li>
+                                    <li>$ {product.price}</li>
+                                    <li>{product.description}</li>
+                                </ul>
+                            </div>
+
+                            <div className="col-1">
+                                <div className="card card-body">
+                                    <ul>
+                                        <li><div className="row center">Price: ${product.price}</div></li>
+                                        <li><div className="row center">Availability:
+                                            {product.countInStock > 0 ? (<span className="success">Stock Available</span>) : (<span className="error">Out of Stock</span>)}
+                                        </div></li>
+
+                                        {product.countInStock > 0 && (
+                                            <>
+                                                <li>
+                                                    <div className="row center">Quantity:
+                                                        <select value={this.state.qty} onChange={(e:any)=>this.setQty(e.target.value)}>
+                                                            {
+                                                                [...Array(product.countInStock).keys()].map((x:any)=>(
+                                                                    <option key={x+1} value={x+1}>{x+1}</option>
+                                                                ))                                                                
+                                                            }
+                                                        </select>
+                                                    </div>
+                                                </li>
+
+                                                <li>
+                                                    <button className="primary block">Add to Cart</button>
+                                                </li>
+                                            </>
+                                        )}
+                                    </ul>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>)
+                }
             </React.Fragment>
         )
     }
 }
 
-const receive = (state:any)=>{
-    return{
+const receive = (state: any) => {
+    return {
         res: state.details
     }
 }
 
-const send = (dispatch:any)=>{
+const send = (dispatch: any) => {
     return {
-        getDetailsById:(id:any)=>{
+        getDetailsById: (id: any) => {
             dispatch(getDetails(id))
         }
     }
 }
 
-export default connect(receive,send)(ProductScreen);
+export default connect(receive, send)(ProductScreen);
